fd889f7fb0e6bd6a256c4d577b47113888ba8db0
Jeffersonm06 <wellingtonalexandre857@gmail.com>
2025-11-13 17:11:39 -0300

    update config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,19 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+// <reference types="@capacitor/keyboard" />
+
+import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'helena',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      style: KeyboardStyle.Dark,
+      resizeOnFullScreen: true,
+    },
+  },
 };
 
 export default config;
